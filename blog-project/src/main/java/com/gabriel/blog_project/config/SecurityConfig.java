4a95c6d9c93af6bcf74447b1157e0978c8e1b53e
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2025-08-23 20:51:03 -0300

    feat: implement Spring Security and add SecurityConfig class to configure route security

---
+++ blog-project/src/main/java/com/gabriel/blog_project/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.gabriel.blog_project.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    return http	
+	        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+	        .csrf(csrf -> csrf.disable()) // Desabilita CSRF (necessário para H2 Console)
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/auth/register", "/auth/login", "/users").permitAll()
+	            .requestMatchers("/posts/**").authenticated()
+	            .anyRequest().authenticated()
+	        )
+	        .sessionManagement(session -> session
+	            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        )
+	        .build();
+	}
+	
+	 @Bean
+	    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+	    	return authConfiguration.getAuthenticationManager();
+	    }
+	    
+	    @Bean
+	    public PasswordEncoder passwordEncoder() {
+	    	return new BCryptPasswordEncoder();
+	    }
+	
+	    @Bean
+		public CorsConfigurationSource corsConfigurationSource() {
+		    CorsConfiguration configuration = new CorsConfiguration();
+		    configuration.setAllowedOrigins(List.of("*")); // Permite apenas o frontend
+		    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+		    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Headers permitidos
+		    configuration.setAllowCredentials(true);
+
+		    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		    source.registerCorsConfiguration("/**", configuration);
+		    return source;
+		}
+
+}
