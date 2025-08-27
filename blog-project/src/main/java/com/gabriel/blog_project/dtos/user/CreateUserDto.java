01a3d5a61ade7f62886f9107c938abb96f3f3cb4
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2025-08-27 17:04:24 -0300

    feat: add input validations with regex in CreateUserDto fields

--- blog-project/src/main/java/com/gabriel/blog_project/dtos/user/CreateUserDto.java
+++ blog-project/src/main/java/com/gabriel/blog_project/dtos/user/CreateUserDto.java
@@ -2,6 +2,29 @@ package com.gabriel.blog_project.dtos.user;
 
 import com.gabriel.blog_project.entities.EnumRole;
 
-public record CreateUserDto(String username, String login, String password, String imageUser, EnumRole role) {
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserDto(
+		@NotBlank(message = "Username cannot be empty")
+	    @Size(max = 255, message = "Username cannot be longer than 200 characters")
+		String username, 
+		@NotBlank(message = "Email cannot be empty")
+        @Size(max = 600, message = "Email cannot be longer than 600 characters")
+		@Pattern(
+				regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+				message = "Invalid email format"
+				)
+		String login, 
+		@Column(nullable = false)
+		@NotBlank(message = "Password cannot be empty")
+		@Size(max = 300, message = "Password cannot be longer than 300 characters")
+		@Pattern(
+				regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])\\S{6,}$",
+		        message = "Password must contain at least one letter, one number, and one special character and not empty spaces"
+		        )
+		String password, String imageUser, EnumRole role) {
 	
 }
