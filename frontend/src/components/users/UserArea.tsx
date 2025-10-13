d5832f0f84eb66346e2df015c8c23ce185e6f6ec
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2025-10-13 13:14:04 -0300

    feat: add UserArea component to display user information

---
+++ frontend/src/components/users/UserArea.tsx
@@ -0,0 +1,54 @@
+// src/components/UserArea.tsx
+import { Link } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
+import Logout from "../../utils/Logout";
+import { useUserData } from "../../utils/useUserData";
+
+interface UserAreaProps {
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+function UserArea({ isSidebarOpen, toggleSidebar }: UserAreaProps) {
+  const { imageUser } = useUserData();
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  return (
+    <>
+      <button className="menu-toggle" onClick={toggleSidebar}>
+        {isSidebarOpen ? "✖" : "☰"}
+      </button>
+
+      <aside className={`sidebar-user ${isSidebarOpen ? "open" : ""}`}>
+        <h2>Área do Usuário</h2>
+        {imageUser ? (
+          <img
+            src={`${API_URL}${imageUser}`}
+            alt="Imagem do perfil"
+            className="profile-picture"
+            style={{ width: 200, height: 200, borderRadius: "50%", objectFit: "cover" }}
+          />
+        ) : (
+          <FaUserCircle size={200} />
+        )}
+
+        <ul>
+          <li>
+            <Link to="/profile" className="dropdown-button">Informações pessoais</Link>
+          </li>
+          <li>
+            <Link to="/statistics-user" className="dropdown-button">Ver estatísticas</Link>
+          </li>
+          <li>
+            <button>Deletar todos os seus posts</button>
+          </li>
+          <li>
+            <Logout />
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
+}
+
+export default UserArea;
