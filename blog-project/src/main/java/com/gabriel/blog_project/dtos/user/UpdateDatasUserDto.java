a92fa8e9f6d98587fc9bd53b1a38d261daf44e96
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2025-12-07 20:49:22 -0300

    feat: allow optional password in UpdateDatasUserDto

--- blog-project/src/main/java/com/gabriel/blog_project/dtos/user/UpdateDatasUserDto.java
+++ blog-project/src/main/java/com/gabriel/blog_project/dtos/user/UpdateDatasUserDto.java
@@ -2,28 +2,24 @@ package com.gabriel.blog_project.dtos.user;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record UpdateDatasUserDto(@NotBlank(message = "Username cannot be empty")
+public record UpdateDatasUserDto(
+	@NotBlank(message = "Username cannot be empty")
 	@Size(max = 255, message = "Username cannot be longer than 200 characters")
 	String username, 
+	
 	@NotBlank(message = "Email cannot be empty")
 	@Size(max = 600, message = "Email cannot be longer than 600 characters")
 	@Pattern(
 			regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
 			message = "Invalid email format"
 			)
+	
 	String login, 
-	@Column(nullable = false)
-	@NotBlank(message = "Password cannot be empty")
-	@Size(max = 300, message = "Password cannot be longer than 300 characters")
-	@Pattern(
-			regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])\\S{6,}$",
-	        message = "Password must contain at least one letter, one number, and one special character and not empty spaces"
-	        )
+	
 	String password, MultipartFile imageUser) {
 
 }
